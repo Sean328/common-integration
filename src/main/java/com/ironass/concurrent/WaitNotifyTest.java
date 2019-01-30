ff314054c6b4183137c80110ab478a9ef0dd1337
sean <seanlee2333@gmail.com>
2019-01-30 22:32:13 +0800

    feat 完善 wait() notify()的测试，需要加锁的

--- src/main/java/com/ironass/concurrent/WaitNotifyTest.java
+++ src/main/java/com/ironass/concurrent/WaitNotifyTest.java
@@ -1,5 +1,6 @@
 package com.ironass.concurrent;
 
+import java.time.LocalDate;
 import java.util.concurrent.Executors;
 
 /**
@@ -12,20 +13,25 @@ public class WaitNotifyTest {
 
     public static void main(String[] args) {
 
+        WaitNotifyTest test = new WaitNotifyTest();
+        test.asyncProcess();
 
-         WaitNotifyTest test = new WaitNotifyTest();
-         test.asyncProcess();
+        System.out.println(LocalDate.now().toString());
 
     }
 
-    private void asyncProcess(){
+    private void asyncProcess() {
 
         Runnable runnable1 = new Runnable() {
             @Override
             public void run() {
                 section1();
-                commonObject.notify();
+                synchronized (commonObject) {
+                    System.out.println("task1获取到了锁");
+                    commonObject.notify();
+                }
             }
+
             private void section1() {
                 System.out.println("task1 执行");
             }
@@ -35,21 +41,52 @@ public class WaitNotifyTest {
             @Override
             public void run() {
                 try {
-                    commonObject.wait();
+                    synchronized (commonObject) {
+                        System.out.println("task2获取到了锁");
+                        //执行到wait()方法后会释放锁
+                        commonObject.wait();
+                    }
+
+                    section2();
+
+                    synchronized (commonObject){
+                        System.out.println("task2 执行完毕 再次获取到了锁");
+                        commonObject.notify();
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                section2();
             }
+
             private void section2() {
                 System.out.println("task2 执行");
             }
         };
 
+        Runnable runnable3 = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (commonObject) {
+                        System.out.println("task3获取到了锁");
+                        commonObject.wait();
+                    }
+                    section3();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            private void section3() {
+                System.out.println("task3 执行");
+            }
+        };
+
+        Executors.defaultThreadFactory().newThread(runnable3).start();
         Executors.defaultThreadFactory().newThread(runnable2).start();
         Executors.defaultThreadFactory().newThread(runnable1).start();
-    }
 
+    }
 
 
 }
