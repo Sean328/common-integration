5a5f73829f5fe59ba225cd7a93c1b4acda686641
sean <seanlee2333@gmail.com>
2019-02-17 00:00:29 +0800

    feat aspectj切面注入实例 完善

---
+++ src/main/java/com/ironass/aop/demo/UserService.java
@@ -0,0 +1,15 @@
+package com.ironass.aop.demo;
+
+/**
+ * @author sean
+ * @date 2019/02/16/23:55
+ **/
+public class UserService {
+
+    public String getDemoData(){
+
+
+        return "demo";
+    }
+
+}
---
+++ src/test/java/ironass/AopTest.java
@@ -0,0 +1,24 @@
+package ironass;
+
+import com.ironass.aop.demo.UserService;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import javax.annotation.Tainted;
+
+/**
+ * @author sean
+ * @date 2019/02/16/23:57
+ **/
+public class AopTest extends BaseSpringTest{
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    public void testAspectJ(){
+        String s = userService.getDemoData();
+        System.out.println(s);
+    }
+
+}
