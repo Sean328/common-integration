a6cb822db1d4c89b30c4e8af82486bcad8eb63ef
sean <seanlee2333@gmail.com>
2019-03-05 23:36:32 +0800

    feat leetcode13

---
+++ src/main/java/com/ironass/leetcode/Solution_13.java
@@ -0,0 +1,125 @@
+package com.ironass.leetcode;
+
+/**
+ * @lc app=leetcode id=13 lang=java
+ *
+ * [13] Roman to Integer
+ *
+ * https://leetcode.com/problems/roman-to-integer/description/
+ *
+ * algorithms
+ * Easy (51.51%)
+ * Total Accepted:    367.5K
+ * Total Submissions: 712.7K
+ * Testcase Example:  '"III"'
+ *
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+ * and M.
+ *
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * For example, two is written as II in Roman numeral, just two one's added
+ * together. Twelve is written as, XII, which is simply X + II. The number
+ * twenty seven is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is
+ * written as IV. Because the one is before the five we subtract it making
+ * four. The same principle applies to the number nine, which is written as IX.
+ * There are six instances where subtraction is used:
+ *
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ *
+ * Given a roman numeral, convert it to an integer. Input is guaranteed to be
+ * within the range from 1 to 3999.
+ *
+ * Example 1:
+ *
+ *
+ * Input: "III"
+ * Output: 3
+ *
+ * Example 2:
+ *
+ *
+ * Input: "IV"
+ * Output: 4
+ *
+ * Example 3:
+ *
+ *
+ * Input: "IX"
+ * Output: 9
+ *
+ * Example 4:
+ *
+ *
+ * Input: "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ *
+ *
+ * Example 5:
+ *
+ *
+ * Input: "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ *
+ */
+
+
+/**
+ * @author lixin
+ * @date 2019-03-05 18:57
+ **/
+public class Solution_13 {
+
+    public int romanToInt(String s) {
+        char [] romanChar = s.toCharArray();
+        int result = 0;
+        int last = 0;
+        for (int i = romanChar.length-1; i>=0;i--){
+            int now = translateRoman2Numer(romanChar[i]);
+            if(now < last){
+                result = result - now;
+            }else{
+                result = result + now;
+            }
+
+            last = now;
+        }
+
+        return result;
+    }
+
+    public int translateRoman2Numer(char s){
+        switch (s) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution_13 solution_13 = new Solution_13();
+        System.out.println(solution_13.romanToInt("MCMXCIV"));
+    }
+}
