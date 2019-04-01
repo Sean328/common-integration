c0eca20685dcc3fb4d36eac524e95e9e5d49a60c
sean <seanlee2333@gmail.com>
2019-04-01 23:11:11 +0800

    feat functioninterface

---
+++ src/main/java/com/ironass/functional/MyChildInterface.java
@@ -0,0 +1,14 @@
+package com.ironass.functional;
+
+/**
+ * 继承函数式接口的MyChildInterface将不再是函数式接口，只是一个标准接口
+ * 因为它包含了两个抽象方法，父类的一个和它自己定义的一个抽象方法
+ * 即使它加上了FunctionInterface注解也会报错
+ * @author lixin
+ * @date 2019-02-15 16:54
+ **/
+
+public interface MyChildInterface extends MyInterface{
+
+    int anotherMethod();
+}
