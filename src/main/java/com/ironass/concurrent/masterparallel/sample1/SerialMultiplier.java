c6c5f8d8c8eca19315f7a42be917a4dd8cc5d261
sean <seanlee2333@gmail.com>
2019-04-26 22:20:52 +0800

    feat 二维数组相乘 串行和并行两种执行效率对比

---
+++ src/main/java/com/ironass/concurrent/masterparallel/sample1/SerialMultiplier.java
@@ -0,0 +1,24 @@
+package com.ironass.concurrent.masterparallel.sample1;
+
+/**
+ * 矩阵相乘类
+ * @author lixin
+ * @date 2019-04-25 15:44
+ **/
+public class SerialMultiplier {
+    public static void multiply (double[][] matrix1, double[][] matrix2,
+                                 double[][] result) {
+        int rows1=matrix1.length;
+        int columns1=matrix1[0].length;
+        int columns2=matrix2[0].length;
+        for (int i=0; i<rows1; i++) {
+            for (int j=0; j<columns2; j++) {
+                result[i][j]=0;
+                for (int k=0; k<columns1; k++) {
+                    result[i][j]+=matrix1[i][k]*matrix2[k][j];
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/ironass/concurrent/masterparallel/sample1/TestMain.java
@@ -0,0 +1,37 @@
+package com.ironass.concurrent.masterparallel.sample1;
+
+import org.springframework.util.StopWatch;
+
+import java.util.Date;
+
+/**
+ * @author lixin
+ * @date 2019-04-25 15:48
+ **/
+public class TestMain {
+
+    public static void main(String[] args) {
+        double matrix1[][] = MatrixGenerator.generate(2000, 2000);
+        double matrix2[][] = MatrixGenerator.generate(2000, 2000);
+        double resultSerial[][]= new double[matrix1.length][matrix2[0].length];
+        Date start=new Date();
+        SerialMultiplier.multiply(matrix1, matrix2, resultSerial);
+        Date end=new Date();
+        System.out.printf("Serial: %d%n",end.getTime()-start.getTime());
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("parralle1");
+        double parallel1[][]= new double[matrix1.length] [matrix2[0].length];
+        ParallelIndividualMultiplier.multiply(matrix1,matrix2,parallel1);
+        stopWatch.stop();
+        System.out.println(stopWatch.getLastTaskTimeMillis());
+
+
+        stopWatch.start("parralle2");
+        double parallel2[][]= new double[matrix1.length] [matrix2[0].length];
+        ParallelRowMultiplier.multiply(matrix1,matrix2,parallel2);
+        stopWatch.stop();
+        System.out.println(stopWatch.getLastTaskTimeMillis());
+
+    }
+}
