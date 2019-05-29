85d81284f43d1a4b51ec84f77836085300d6aa0a
sean <seanlee2333@gmail.com>
2019-05-29 23:16:02 +0800

    feat leetcode 100,101

---
+++ src/main/java/com/ironass/leetcode/Solution_100.java
@@ -0,0 +1,108 @@
+package com.ironass.leetcode;
+
+//Given two binary trees, write a function to check if they are the same or not.
+//
+// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+//
+// Example 1:
+//
+//
+//Input:     1         1
+//          / \       / \
+//         2   3     2   3
+//
+//        [1,2,3],   [1,2,3]
+//
+//Output: true
+//
+//
+// Example 2:
+//
+//
+//Input:     1         1
+//          /           \
+//         2             2
+//
+//        [1,2],     [1,null,2]
+//
+//Output: false
+//
+//
+// Example 3:
+//
+//
+//Input:     1         1
+//          / \       / \
+//         2   1     1   2
+//
+//        [1,2,1],   [1,1,2]
+//
+//Output: false
+//
+//
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ *
+ * @author lixin
+ * @date 2019-05-29 14:45
+ **/
+public class Solution_100 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 思路：常见思路是 选取某种遍历方式分别遍历两颗二叉树，遍历结果存放在容器当中并对容器内值进行比较(应该摒弃掉，遍历过程中比较即可)。
+     * 另外一种思路则是通过递归方式，层次遍历两棵树并比较同样节点，有不同的则返回false,这种方式目前来看是该题的最优解。
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null && q != null) {
+            if (p.val != q.val) {
+                return false;
+            } else {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeA1 = new TreeNode(1);
+        TreeNode treeA2 = new TreeNode(2);
+        TreeNode treeA3 = new TreeNode(3);
+        treeA1.left = treeA2;
+        treeA1.right = treeA3;
+
+        TreeNode treeB1 = new TreeNode(1);
+        TreeNode treeB2 = new TreeNode(2);
+        TreeNode treeB3 = new TreeNode(3);
+        treeB1.left = treeB2;
+        treeB1.right = treeB3;
+
+        Solution_100 solution_100 = new Solution_100();
+        System.out.println(solution_100.isSameTree(treeA1, treeB1));
+    }
+}
---
+++ src/main/java/com/ironass/leetcode/Solution_101.java
@@ -0,0 +1,96 @@
+package com.ironass.leetcode;
+//Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+//
+// For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+//
+//
+//    1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+//
+//
+//
+//
+// But the following [1,2,2,null,3,null,3] is not:
+//
+//
+//    1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+//
+//
+// Note:
+//Bonus points if you could solve it both recursively and iteratively.
+//
+
+/**
+ * 判断一棵树是否为镜像树
+ *
+ * @author lixin
+ * @date 2019-05-29 17:46
+ **/
+public class Solution_101 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 思路：解法有多种，题目要求使用递归+迭代的方法更能加分。
+     * 采用递归+迭代的方法 需要新定义一个用于递归迭代的方法，特别需要注意递归调用时的参数传递,参数也是以镜像的方式传递。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            return (isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left));
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree1 = new TreeNode(1);
+        TreeNode tree2 = new TreeNode(2);
+        TreeNode tree3 = new TreeNode(2);
+        TreeNode tree4 = new TreeNode(3);
+        TreeNode tree5 = new TreeNode(3);
+        TreeNode tree6 = new TreeNode(4);
+        TreeNode tree7 = new TreeNode(4);
+
+        tree1.left = tree2;
+        tree1.right = tree3;
+        tree2.left = tree4;
+        tree2.right = tree6;
+        tree3.left = tree7;
+        tree3.right = tree5;
+        Solution_101 solution_101 = new Solution_101();
+        System.out.println(solution_101.isSymmetric(tree1));
+    }
+}
